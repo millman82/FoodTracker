6dc3376cfd667b038f2eba5bb16f4e61e0798775
Tim Miller <millman82@gmail.com>
2016-05-22 21:23:35 -0400

    Initial check-in for Getting Started Developing iOS Apps (Swift) tutorial.

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,15 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Timothy Miller on 5/22/16.
+//  Copyright © 2016 Timothy Miller. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    
+
+}
